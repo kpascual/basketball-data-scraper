0e99255e1b098f5c73b393fefd5d3548287bbef1
Kenneth Pascual <vorped@gmail.com>
2014-12-12 00:01:23 -0800

    new table referee_statsnbacom in schema

--- schema/core_schema.sql
+++ schema/core_schema.sql
@@ -798,6 +798,20 @@ CREATE TABLE `player_statsnbacom` (
   PRIMARY KEY (`statsnbacom_player_id`,`game_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `referee_statsnbacom`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `referee_statsnbacom` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `game_id` int(11) DEFAULT NULL,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `jersey_number` int(11) DEFAULT NULL,
+  `statsnbacom_official_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_game_jersey` (`game_id`,`jersey_number`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `league`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
